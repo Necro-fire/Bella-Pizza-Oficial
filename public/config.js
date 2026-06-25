12461ab262fcf01b97dfff6c0aabbb14ce0a6672
Necro-fire <paulohenrique72541@gmail.com>
2026-06-25 10:18:03 -0300

    DB-Restauração

---
+++ public/config.js
@@ -0,0 +1,4 @@
+window.__SUPABASE_CONFIG__ = {
+  url: 'https://rgvmzvhysduhtuntxjho.supabase.co',
+  publishableKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJndm16dmh5c2R1aHR1bnR4amhvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUwODA2MDAsImV4cCI6MjA5MDY1NjYwMH0.91RaUhtl-tTU-v_r85TJWXmPI_eO5f8d-fAbmKE3MD4'
+};
